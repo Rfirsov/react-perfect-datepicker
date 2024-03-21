c191f90ae0a75163b31398540dcb232955a92a15
Roman Firsov <froma2015@gmail.com>
2024-03-21 21:52:37 +0200

    Remove unused utils.

--- src/utils/timeformat.ts
+++ src/utils/timeformat.ts
@@ -1,8 +1,4 @@
-import { getYear } from "date-fns";
+import { getYear } from 'date-fns';
 
 export const currentTime = new Date();
-
 export const getCurrentYear = getYear(currentTime);
-// export const getCurrentMonth = getMonth(currentTime);
-// export const currentDay = currentTime.getDate();
-// export const currentWeekDay = currentTime.getDay();
